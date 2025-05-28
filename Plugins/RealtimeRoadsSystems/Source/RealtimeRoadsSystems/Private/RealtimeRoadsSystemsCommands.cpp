a64fd207a2b457b5f38163ad02784950b77aee44
Morgan <hello@morgan.games>
2025-05-28 17:56:00 +0100

    push before modifying toolbar

--- Plugins/RealtimeRoadsSystems/Source/RealtimeRoadsSystems/Private/RealtimeRoadsSystemsCommands.cpp
+++ Plugins/RealtimeRoadsSystems/Source/RealtimeRoadsSystems/Private/RealtimeRoadsSystemsCommands.cpp
@@ -1,4 +1,5 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
+// Copyright (c) 2025 Morgan Skillicorn. All rights reserved.
 
 #include "RealtimeRoadsSystemsCommands.h"
 
@@ -7,6 +8,7 @@
 void FRealtimeRoadsSystemsCommands::RegisterCommands()
 {
 	UI_COMMAND(PluginAction, "RealtimeRoadsSystems", "Execute RealtimeRoadsSystems action", EUserInterfaceActionType::Button, FInputChord());
+	UI_COMMAND(StartHAPIServerAction, "Start HAPI Server", "Start the Houdini Engine HAPI session", EUserInterfaceActionType::Button, FInputChord());
 }
 
 #undef LOCTEXT_NAMESPACE
